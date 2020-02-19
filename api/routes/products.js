cc57e07867bc211ba7af211670ce3131dcf6957e
Kenji Miyashiro <funtret12@gmail.com>
2020-02-19 00:42:38 -0500

    creado post con mongoose

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+
+const Product = require("../models/product");
 
 router.get("/", (req, res, next) => {
     res.status(200).json({
@@ -8,28 +11,35 @@ router.get("/", (req, res, next) => {
 });
 
 router.post("/", (req, res, next) => {
-    const product = {
+    const product = new Product({
+        _id: new mongoose.Types.ObjectId(),
         name: req.body.name,
         price: req.body.price
-    };
+    });
+    product.save().then(result => {
+        console.log(result);
+    })
+    .catch(error => {
+        console.log(error);
+    });
     res.status(200).json({
-        message : "handling GET reqest to /products",
+        message : "handling POST request to /products",
         createdProduct: product
     });
 });
 
 router.get("/:productId", (req, res, next) => {
     const id = req.params.productId;
-    if(id === "special") {
-        res.status(200).json({
-            message : "You discovered the special id",
-            id : id
-        });
-    } else {
-        res.status(200).json({
-            message : "You passed an id"
-        });
-    }
+    Product.findById(id)
+    .exec()
+    .then(doc => {
+        console.log(doc);
+        res.status(200).json(doc);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({error: err});
+    });
 });
 
 router.patch("/:productId", (req, res, next) => {
